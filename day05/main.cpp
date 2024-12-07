a024b2d641eb6ea10de305a052ccb85911d892a1
elecarlier <eleonorecarlier@hotmail.be>
2024-12-07 17:30:36 +0100

    task 2

--- day05/main.cpp
+++ day05/main.cpp
@@ -11,7 +11,9 @@ using namespace std;
 void print_vector_int(vector<vector<int>> numbersLines);
 bool check_temp(vector<pair<int, int>> pairs, vector<int> vec);
 void check_invalid(vector<pair<int, int>> pairs, vector<vector<int>> numbersLines);
-void calc_res(vector<vector<int>> valid);
+void calc_res(vector<vector<int>> valid, bool task);
+vector<int> check_and_swap(vector<pair<int, int>> pairs, vector<int>& vec);
+void task2(vector<pair<int, int>> pairs, vector<vector<int>> invalid);
 
 int main() {
 	vector<pair<int, int>> pairsLines;
@@ -72,32 +74,49 @@ void print_vector_int(vector<vector<int>> numbersLines)
 void check_invalid(vector<pair<int, int>> pairs, vector<vector<int>> numbersLines)
 {
 	vector<vector<int>> valid;
+	vector<vector<int>> invalid;
 	for (vector<vector<int>>::iterator lineIt = numbersLines.begin(); lineIt != numbersLines.end(); ++lineIt)
 	{
 		bool result = check_temp(pairs, *lineIt);
 		if (result)
-		{
-			//cout << "Ligne valide!" << endl;
 			valid.push_back(*lineIt);
-		}
+		else
+			invalid.push_back(*lineIt);
+
 	}
-	calc_res(valid);
+
+	calc_res(valid, 1);
+	task2(pairs, invalid);
 }
 
+void task2(vector<pair<int, int>> pairs, vector<vector<int>> invalid)
+{
+	vector<vector<int>> new_valid;
+	vector<int> temp;
+	for (vector<vector<int>>::iterator lineIt = invalid.begin(); lineIt != invalid.end(); ++lineIt)
+	{
+		temp = check_and_swap(pairs, *lineIt);
+		new_valid.push_back(temp);
 
-void calc_res(vector<vector<int>> valid)
+	}
+	calc_res(new_valid, 0);
+
+}
+
+void calc_res(vector<vector<int>> valid, bool task)
 {
 	int res = 0;
 
-	cout << "size of valid : " << valid.size();
 	for (vector<vector<int>>::iterator lineIt = valid.begin(); lineIt != valid.end(); ++lineIt) {
 		vector<int> line = *lineIt;
 		int middleIndex = line.size() / 2;
 		res += line[middleIndex];
 	}
 
-	std::cout << "Somme des milieurs : " << res << endl;
-
+	if (task)
+		std::cout << "Task 1 : " << res << endl;
+	else
+		std::cout << "Task 2: " << res << endl;
 }
 
 bool check_temp(vector<pair<int, int>> pairs, vector<int> vec)
@@ -115,11 +134,6 @@ bool check_temp(vector<pair<int, int>> pairs, vector<int> vec)
 			inval_pairs.push_back(p);
 		}
 	}
-	// cout << "inval_pairs: ";
-	// for (const auto& pair : inval_pairs) {
-	// 	cout << "(" << pair.first << ", " << pair.second << ") ";
-	// }
-	// cout << endl;
 
 	for (const auto& inval_pair : inval_pairs)
 	{
@@ -127,4 +141,60 @@ bool check_temp(vector<pair<int, int>> pairs, vector<int> vec)
 			return false;
 	}
 	return true;
+}
+
+// vector<int> check_and_swap(vector<pair<int, int>> pairs, vector<int>& vec)
+// {
+// 	vector<pair<int, int>> inval_pairs;
+
+// 	for (vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+// 	{
+// 		for (vector<int>::iterator jt = it + 1; jt != vec.end(); ++jt)
+// 		{
+// 			pair<int, int> p(*it, *jt);
+// 			inval_pairs.push_back(p);
+// 		}
+// 	}
+
+// 	for (vector<pair<int, int>>::iterator inval_it = inval_pairs.begin(); inval_it != inval_pairs.end(); ++inval_it)
+// 	{
+// 		if (find(pairs.begin(), pairs.end(), *inval_it) == pairs.end()) {
+// 			//cout << "Paire invalide trouvée: (" << inval_it->first << ", " << inval_it->second << ")" << endl;
+// 			vector<int>::iterator it1 = find(vec.begin(), vec.end(), inval_it->first);
+// 			vector<int>::iterator it2 = find(vec.begin(), vec.end(), inval_it->second);
+
+// 			if (it1 != vec.end() && it2 != vec.end())
+// 				swap(*it1, *it2);
+
+// 	}
+// 	}
+// 	return vec;
+// }
+
+vector<int> check_and_swap(vector<pair<int, int>> pairs, vector<int>& vec)
+{
+	vector<pair<int, int>> inval_pairs;
+
+	for (vector<int>::iterator it = vec.begin(); it != vec.end(); ++it)
+	{
+		for (vector<int>::iterator jt = it + 1; jt != vec.end(); ++jt)
+		{
+			pair<int, int> p(*it, *jt);
+			inval_pairs.push_back(p);
+		}
+	}
+
+	// Reorder the invalid pairs
+	sort(vec.begin(), vec.end(), [&pairs](int a, int b)
+	{
+		for (const auto& pair : pairs) {
+			if (pair.first == a && pair.second == b)
+				return true;
+			if (pair.first == b && pair.second == a)
+				return false;
+		}
+		return false; // If no pair found, maintain order
+	});
+
+	return vec;
 }
