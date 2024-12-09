d4b7c8ffcbcb4a5932df37312d5251032907798e
elecarlier <eleonorecarlier@hotmail.be>
2024-12-09 15:58:13 +0100

    pars input

--- day09/main.cpp
+++ day09/main.cpp
@@ -8,11 +8,27 @@
 #include <map>
 #include <set>
 
+
+void print_vector(std::vector<std::string> vec)
+{
+
+    if (vec.empty())
+        std::cout << "Empty" << std::endl;
+    else
+    {
+    	for (std::string num : vec)
+		{
+			std::cout << num << " " ;
+		}
+
+    }
+
+}
 void parse_input(std::string input);
 
 int main()
 {
-    //std::ifstream inputFile("input.txt");
+	//std::ifstream inputFile("input.txt");
 	std::ifstream inputFile("mininput.txt");
 
 	if (!inputFile.is_open()) {
@@ -21,40 +37,50 @@ int main()
 	}
 
 	std::string line;
-
-    if (getline(inputFile, line))
-		std::cout << line << std::endl;
-	else
-		std::cerr << "Fichier vide ou erreur" << std::endl;
+	getline(inputFile, line);
+	// if (getline(inputFile, line))
+	// 	std::cout << line << std::endl;
+	// else
+	// 	std::cerr << "Fichier vide ou erreur" << std::endl;
 
 	inputFile.close();
-
+	std::cout << line << std::endl;
 	parse_input(line);
-    
+
 	return 0;
 }
 
 void parse_input(std::string input)
 {
-
 	int index = 0;
-	std::string blocks;
+	//std::string blocks;
+	std::vector<std::string> blocks;
+	std::string index_as_string;
 
-	for (long unsigned int i = 0; i < input.length(); i++)
+	for (int i = 0; i < int(input.length()); i++)
 	{
 		if (i % 2 == 0) //pair
 		{
-			std::string index_string = index - '0';
-			for (int j = 0; j < input[i] ; j++)
-				blocks.push_back(index_string);
+			std::stringstream stream;
+			stream << index;
+			stream >> index_as_string;
+			int j = 0;
+			while (j < (input[i]- '0'))
+			{
+				blocks.push_back(index_as_string);
+				j++;
+			}
 			index++;
 		}
-		else 
+		else
 		{
-			for (int j = 0; j < input[i] ; j++)
-				blocks.push_back('.');
+			int j = 0;
+			while (j < int(input[i] - '0'))
+			{
+				blocks.push_back("-1");
+				j++;
+			}
 		}
-
 	}
-	std::cout << blocks << std::endl;
+	print_vector(blocks);
 }
